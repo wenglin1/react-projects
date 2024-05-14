174eca58f86bd0949f4bc1317e6637266b3da7f6
weng lin <wenglin@buffalo.edu>
2024-05-14 19:57:37 -0400

    image slider done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import './App.css';
-import Accordian from './components/accordian';
-import RandomColor from './components/random-color';
-import StarRating from './components/star-rating';
+import "./App.css";
+import Accordian from "./components/accordian";
+import ImageSlider from "./components/image-slider";
+import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
 
 function App() {
   return (
     <div className="App">
       {/* Accordian component */}
-      <Accordian/>
+      <Accordian />
       {/* Random color component */}
-      <RandomColor/>
+      <RandomColor />
       {/* Star rating component*/}
-      <StarRating/>
+      <StarRating noOfStars={10} />
+      {/* Image slider componenet */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </div>
   );
 }
--- src/components/star-rating/index.jsx
+++ src/components/star-rating/index.jsx
@@ -1,44 +1,41 @@
 // ask the interview how many stars they want
 
-import { FaStar } from "react-icons/fa"
-import { useState } from "react"
-import "./styles.css"
+import { FaStar } from "react-icons/fa";
+import { useState } from "react";
+import "./styles.css";
 
+export default function StarRating({ noOfStars = 1 }) {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
 
-export default function StarRating({noOfStars = 10}) {
+  function handleClick(getCurrentIndex) {
+    setRating(getCurrentIndex);
+  }
 
-    const [rating, setRating] = useState(0)
-    const [hover, setHover] = useState(0)
+  function handleMouseEnter(getCurrentIndex) {
+    setHover(getCurrentIndex);
+  }
 
-    function handleClick(getCurrentIndex) {
-        setRating(getCurrentIndex)
-        
-    }
+  function handleMouseLeave() {
+    setHover(rating);
+  }
 
-    function handleMouseEnter(getCurrentIndex) {
-       setHover(getCurrentIndex)
-    }
-
-    function handleMouseLeave() {
-        setHover(rating)
-    }
-
-
-    return (
-        <div className="star-rating">
-            {
-                [...Array(noOfStars)].map((_,index)=> {
-                    index+=1
-                    return (<FaStar 
-                        key={index}
-                        className = {index <= (hover || rating) ? "active" : "inactive"}
-                        onClick={() => handleClick(index)}
-                        onMouseMove={() => handleMouseEnter(index)}
-                        onMouseLeave={() => handleMouseLeave(index)}
-                        size={40}
-                    />
-                )
-            })}
-        </div>
-    )
+  return (
+    <div className="container">
+      {[...Array(noOfStars)].map((_, index) => {
+        index += 1;
+        return (
+          <FaStar
+            key={index}
+            className={index <= (hover || rating) ? "active" : "inactive"}
+            onClick={() => handleClick(index)}
+            onMouseMove={() => handleMouseEnter(index)}
+            onMouseLeave={() => handleMouseLeave(index)}
+            size={40}
+          />
+        );
+      })}
+      <div>Rating: {rating}</div>
+    </div>
+  );
 }
